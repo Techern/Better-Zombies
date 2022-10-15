cefc715bf3b5333d2fa2359cb212c5c69331da9e
Cruz Bishop <cruz@techern.org>
2022-10-15 20:25:18 +1000

    Remove ZombieType and work on debug

--- src/main/java/org/techern/betterzombies/events/InteractWithZombiesEventHandler.java
+++ src/main/java/org/techern/betterzombies/events/InteractWithZombiesEventHandler.java
@@ -1,11 +1,11 @@
 package org.techern.betterzombies.events;
 
-import com.mojang.brigadier.LiteralMessage;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.techern.betterzombies.ZombieType;
 
 /**
  * Handles all {@link PlayerInteractEvent.EntityInteractSpecific}s and filters out zombie-related events to {@code #playerInteractsWithZombie}
@@ -22,18 +22,66 @@ public class InteractWithZombiesEventHandler {
      * @since 0.1
      */
     @SubscribeEvent
-    public static void playerInteractsWithAnything(PlayerInteractEvent.EntityInteractSpecific event){
-        event.getEntity().sendSystemMessage(Component.literal("Hello, you clicked on a " + event.getTarget().getName().getString() + " with a " + event.getEntity().getItemInHand(event.getHand()).getDisplayName().getString()));
+    public static void playerInteractsWithAnything(PlayerInteractEvent.EntityInteractSpecific event) {
+
+        //We only need to process this in the server side
+        if (event.getSide().isClient()) {
+            return;
+        }
+
+        if (event.getEntity().getItemInHand(event.getHand()).getItem().equals(Items.GOLDEN_APPLE)) {
+
+            EntityType<?> type = event.getTarget().getType();
+            if (type.equals(EntityType.ZOMBIE) || type.equals(EntityType.HUSK) || type.equals(EntityType.DROWNED)) {
+                playerInteractsWithZombie(event, event.getTarget().getType());
+            } else if (EntityType.ZOMBIE_HORSE.equals(type)) {
+                playerInteractsWithZombieHorse(event);
+            } else if (EntityType.ZOMBIE_VILLAGER.equals((type))) {
+                playerInteractsWithZombieVillager(event);
+            } else if (EntityType.ZOMBIFIED_PIGLIN.equals((type))) {
+                playerInteractsWithZombiePiglin(event);
+            }
+        }
     }
 
     /**
-     * Only called whenever a {@link PlayerInteractEvent.EntityInteractSpecific} is for a {@link ZombieType}
+     * Only called whenever a {@link PlayerInteractEvent.EntityInteractSpecific} is for a {@link EntityType} that is a zombie mob
      *
      * @param event The {@link PlayerInteractEvent}
      * @param zombieType The type of zombie
      * @since 0.1
      */
-    protected static void playerInteractsWithZombie(PlayerInteractEvent.EntityInteractSpecific event, ZombieType zombieType) {
+    protected static void playerInteractsWithZombie(PlayerInteractEvent.EntityInteractSpecific event, EntityType zombieType) {
+        event.getEntity().sendSystemMessage(Component.literal("You tried to cure a zombie that isn't a villager"));
+    }
 
+    /**
+     * Only called whenever a {@link PlayerInteractEvent.EntityInteractSpecific} is for a {@link EntityType} that is a zombie horse
+     *
+     * @param event The {@link PlayerInteractEvent}
+     * @since 0.1
+     */
+    protected static void playerInteractsWithZombieHorse(PlayerInteractEvent.EntityInteractSpecific event) {
+        event.getEntity().sendSystemMessage(Component.literal("that's a horse"));
+    }
+
+    /**
+     * Only called whenever a {@link PlayerInteractEvent.EntityInteractSpecific} is for a {@link EntityType} that is a zombie villager
+     *
+     * @param event The {@link PlayerInteractEvent}
+     * @since 0.1
+     */
+    protected static void playerInteractsWithZombieVillager(PlayerInteractEvent.EntityInteractSpecific event) {
+        event.getEntity().sendSystemMessage(Component.literal("YES!"));
+    }
+
+    /**
+     * Only called whenever a {@link PlayerInteractEvent.EntityInteractSpecific} is for a {@link EntityType} that is a zoglin mob
+     *
+     * @param event The {@link PlayerInteractEvent}
+     * @since 0.1
+     */
+    protected static void playerInteractsWithZombiePiglin(PlayerInteractEvent.EntityInteractSpecific event) {
+        event.getEntity().sendSystemMessage(Component.literal("*pig noises*"));
     }
 }
