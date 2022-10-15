1302e6ba8027ed53f90dd2216182d7354ae64576
Cruz Bishop <cruz@techern.org>
2022-10-16 09:17:35 +1000

    Release 0.1 for Forge

--- src/main/java/org/techern/betterzombies/BetterZombies.java
+++ src/main/java/org/techern/betterzombies/BetterZombies.java
@@ -1,17 +1,12 @@
 package org.techern.betterzombies;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(BetterZombies.MODID)
 public class BetterZombies
 {
     public static final String MODID = "betterzombies";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public BetterZombies()
     {
