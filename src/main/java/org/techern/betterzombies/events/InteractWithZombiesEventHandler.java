66f9a4e7f28707aa79cd9fae69e995d0758e9741
Cruz Bishop <cruz@techern.org>
2022-10-16 09:06:44 +1000

    Clean up preparing for release
    Don't need a lot of this boilerplate

--- src/main/java/org/techern/betterzombies/events/InteractWithZombiesEventHandler.java
+++ src/main/java/org/techern/betterzombies/events/InteractWithZombiesEventHandler.java
@@ -1,8 +1,6 @@
 package org.techern.betterzombies.events;
 
-import joptsimple.internal.Reflection;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -10,7 +8,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.SpawnGroupData;
-import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.animal.horse.ZombieHorse;
 import net.minecraft.world.entity.monster.*;
